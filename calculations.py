abd7bc8c66922f5a537909244aaee8b1bc4cee2d
Tommi-Haipola <tommihaipola@gmail.com>
2024-04-16 01:02:57 +1000

    mat mul shape errors to be fixed

--- calculations.py
+++ calculations.py
@@ -1,7 +1,17 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 predict_df = pd.read_csv('./result/predict.csv',header=None)
 true_df = pd.read_csv('./result/true.csv',header=None)
 
-print(predict_df.iloc[:,4])
+
+pred = predict_df.iloc[:,0].to_numpy()
+idx = true_df.index
+true = true_df.to_numpy()
+print(predict_df.iloc[:,0])
 print(true_df)
+
+plt.plot(idx,pred)
+plt.plot(idx,true)
+plt.show()
+
